a31cef1d7674c9931c70158b2579c997150839d4
Esneyder Monsalve <esneyder110@gmail.com>
2023-12-14 09:23:16 -0400

    feat: Set Up Tailwind Theme Colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-light dark:bg-dark text-dark  dark:text-primary`}>
+        {children}
+      </body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
 import type { Config } from 'tailwindcss'
-
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#FFFFFF",
+        secondary: "#A7A8AB",
+        tertiary: '#1D1D1D',
+        accentPrimary: "#7A23B9",
+        accentSecondary: "#C58A42",
+        accentTertiary: "#C58A42",
+        accent: "#6B72FA",
+        dark: "#262525",
+        light: "#FFFFFF",
+        // You can add more colors as needed
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
